9946594011226ad2d47ba79f8380f4aaa092b2d9
topmbr <centerm9911@gmail.com>
2023-12-24 10:49:27 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp103/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+namespace ConsoleApp103
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                // Устанавливаем порт для прослушивания клиентов
+                int port = 8888;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(localAddr, port);
+
+                server.Start();
+
+                Console.WriteLine("Ожидание подключений...");
+
+                // Генерируем случайное число от 1 до 10
+                Random random = new Random();
+                int secretNumber = random.Next(1, 5);
+
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Клиент подключен!");
+
+                    NetworkStream stream = client.GetStream();
+
+                    // Приветственное сообщение
+                    byte[] welcomeMessage = Encoding.UTF8.GetBytes("Добро пожаловать в игру 'Угадай число'!\n");
+                    stream.Write(welcomeMessage, 0, welcomeMessage.Length);
+
+                    // Получаем имя игрока
+                    byte[] data = new byte[256];
+                    int bytes = stream.Read(data, 0, data.Length);
+                    string playerName = Encoding.UTF8.GetString(data, 0, bytes);
+                    Console.WriteLine($"Игрок {playerName} присоединился к игре.");
+
+                    // Отправляем сообщение секретного числа
+                    byte[] secretNumberMessage = Encoding.UTF8.GetBytes($"Загаданное число от 1 до 5. Попробуйте угадать!\n");
+                    stream.Write(secretNumberMessage, 0, secretNumberMessage.Length);
+
+                    bool guessed = false;
+                    while (!guessed)
+                    {
+                        // Получаем догадку игрока
+                        bytes = stream.Read(data, 0, data.Length);
+                        string guess = Encoding.UTF8.GetString(data, 0, bytes);
+
+                        // Проверяем догадку
+                        int playerGuess;
+                        if (int.TryParse(guess, out playerGuess))
+                        {
+                            if (playerGuess == secretNumber)
+                            {
+                                // Игрок угадал число
+                                byte[] successMessage = Encoding.UTF8.GetBytes($"Поздравляем, {playerName}! Вы угадали число {secretNumber}!\n");
+                                stream.Write(successMessage, 0, successMessage.Length);
+                                guessed = true;
+                            }
+                            else
+                            {
+                                // Сравниваем числа и отправляем подсказку
+                                string hint = (playerGuess < secretNumber) ? "Число больше." : "Число меньше.";
+                                byte[] hintMessage = Encoding.UTF8.GetBytes(hint + "\n");
+                                stream.Write(hintMessage, 0, hintMessage.Length);
+                            }
+                        }
+                        else
+                        {
+                            // Некорректный ввод
+                            byte[] errorMessage = Encoding.UTF8.GetBytes("Некорректный ввод. Пожалуйста, введите число от 1 до 5.\n");
+                            stream.Write(errorMessage, 0, errorMessage.Length);
+                        }
+                    }
+
+                    // Закрываем соединение с клиентом
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Закрываем сервер
+                server?.Stop();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Подключаемся к серверу
+                TcpClient client = new TcpClient("127.0.0.1", 8888);
+                NetworkStream stream = client.GetStream();
+
+                // Вводим имя игрока
+                Console.Write("Введите свое имя: ");
+                string playerName = Console.ReadLine();
+
+                // Отправляем имя серверу
+                byte[] playerNameBytes = Encoding.UTF8.GetBytes(playerName);
+                stream.Write(playerNameBytes, 0, playerNameBytes.Length);
+
+                // Получаем приветственное сообщение от сервера
+                byte[] welcomeMessage = new byte[256];
+                int bytesRead = stream.Read(welcomeMessage, 0, welcomeMessage.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(welcomeMessage, 0, bytesRead));
+
+                while (true)
+                {
+                    // Вводим догадку
+                    Console.Write("Введите число от 1 до 5: ");
+                    string guess = Console.ReadLine();
+
+                    // Отправляем догадку серверу
+                    byte[] guessBytes = Encoding.UTF8.GetBytes(guess);
+                    stream.Write(guessBytes, 0, guessBytes.Length);
+
+                    // Получаем ответ от сервера
+                    byte[] response = new byte[256];
+                    bytesRead = stream.Read(response, 0, response.Length);
+                    Console.WriteLine(Encoding.UTF8.GetString(response, 0, bytesRead));
+
+                    // Проверяем, угадал ли игрок число
+                    if (Encoding.UTF8.GetString(response, 0, bytesRead).Contains("Поздравляем"))
+                    {
+                        Console.WriteLine("Вы выиграли! Нажмите Enter для завершения.");
+                        Console.ReadLine();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
